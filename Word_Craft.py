87a4c037a2a4011221dea1d06ad34db3bbd227fa
Tanishq Pokharia <tppokharia@gmail.com>
2023-10-09 23:47:58 +0530

    Update Word_Craft.py

--- Word_Craft.py
+++ Word_Craft.py
@@ -12,240 +12,46 @@ x = (root.winfo_screenwidth()/2)-(820/2)
 y = (root.winfo_screenheight()/2)-(700/2)
 root.geometry(f"{820}x{700}+{int(x)}+{int(y)}")
 
+button_list = []
+ms=0
+submit_count=0
+Score=0
+Score_string=f"Score: {Score}"
+reset_counter=0
+colour_list=["red","light blue","light green","violet","magenta","white","yellow","blue","indigo","cyan","pink","green","orange"]
+frames = [
+    PhotoImage(file=r"welcome.gif", format="gif -index %i" % (i))
+    for i in range(2)
+]
+
+def create_button(letter, row, col):
+    button = Button(f_two, text=letter, width=13, height=3, command=lambda: words(letter, row * 8 + col), bg="light blue")
+    button.place(x=10 + col * 100, y=150 + row * 50)
+    return button
+
+
 def play():
     global submit_count
     f_one.place_forget()
     f_two.place(x=0, y=0)
     global e_one
-    e_one=Entry(f_two,width=30,font="20",borderwidth=10,state='readonly')
+    e_one=Entry(f_two, width=30, font="20",borderwidth=10,state='readonly')
     e_one.place(x=240,y=50)
-    text_one=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_two=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_three=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_four=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_five=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_six=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_seven=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_eight=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_nine=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_ten=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_eleven=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twelve=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirteen=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_fourteen=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_fifteen=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_sixteen=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_seventeen=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_eighteen=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_nineteen=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twenty=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentyone=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentytwo=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentythree=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentyfour=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentyfive=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentysix=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentyseven=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentyeight=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_twentynine=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirty=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtyone=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtytwo=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtythree=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtyfour=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtyfive=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtysix=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtyseven=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtyeight=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_thirtynine=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_forty=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_fortyone=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_fortytwo=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_fortythree=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    text_fortyfour=ascii_lowercase[random.randrange(len(ascii_lowercase))]
-    
-    
-    
-    
-    global b_one
-    b_one=Button(f_two,text=text_one,width=13,height=3,command=lambda: words(text_one,0),bg="light blue")
-    global b_two
-    
-    b_two=Button(f_two,text=text_two,width=13,height=3,command=lambda: words(text_two,1),bg="light blue")
-    global b_three
-    b_three=Button(f_two,text=text_three,width=13,height=3,command=lambda: words(text_three,2),bg="light blue")
-    global b_four
-    
-    b_four=Button(f_two,text=text_four,width=13,height=3,command=lambda: words(text_four,3),bg="light blue")
-    global b_five
-    
-    b_five=Button(f_two,text=text_five,width=13,height=3,command=lambda: words(text_five,4),bg="light blue")
-    global b_six
-    
-    b_six=Button(f_two,text=text_six,width=13,height=3,command=lambda: words(text_six,5),bg="light blue")
-    global b_seven
-    
-    b_seven=Button(f_two,text=text_seven,width=13,height=3,command=lambda: words(text_seven,6),bg="light blue")
-    global b_eight
-    
-    b_eight=Button(f_two,text=text_eight,width=13,height=3,command=lambda: words(text_eight,7),bg="light blue")
-    global b_nine
-    
-    b_nine=Button(f_two,text=text_nine,width=13,height=3,command=lambda: words(text_nine,8),bg="light blue")
-    global b_ten
-    
-    b_ten=Button(f_two,text=text_ten,width=13,height=3,command=lambda: words(text_ten,9),bg="light blue")
-    global b_eleven
-    
-    b_eleven=Button(f_two,text=text_eleven,width=13,height=3,command=lambda: words(text_eleven,10),bg="light blue")
-    global b_twelve
-    
-    b_twelve=Button(f_two,text=text_twelve,width=13,height=3,command=lambda: words(text_twelve,11),bg="light blue")
-    global b_thirteen
-    
-    b_thirteen=Button(f_two,text=text_thirteen,width=13,height=3,command=lambda: words(text_thirteen,12),bg="light blue")
-    global b_fourteen
-    
-    b_fourteen=Button(f_two,text=text_fourteen,width=13,height=3,command=lambda: words(text_fourteen,13),bg="light blue")
-    global b_fifteen
-    
-    b_fifteen=Button(f_two,text=text_fifteen,width=13,height=3,command=lambda: words(text_fifteen,14),bg="light blue")
-    global b_sixteen
-    
-    b_sixteen=Button(f_two,text=text_sixteen,width=13,height=3,command=lambda: words(text_sixteen,15),bg="light blue")
-    global b_seventeen
-    
-    b_seventeen=Button(f_two,text=text_seventeen,width=13,height=3,command=lambda: words(text_seventeen,16),bg="light blue")
-    global b_eighteen
-    
-    b_eighteen=Button(f_two,text=text_eighteen,width=13,height=3,command=lambda: words(text_eighteen,17),bg="light blue")
-    global b_nineteen
-    
-    b_nineteen=Button(f_two,text=text_nineteen,width=13,height=3,command=lambda: words(text_nineteen,18),bg="light blue")
-    global b_twenty
-    
-    b_twenty=Button(f_two,text=text_twenty,width=13,height=3,command=lambda: words(text_twenty,19),bg="light blue")
-    global b_twentyone
-    
-    b_twentyone=Button(f_two,text=text_twentyone,width=13,height=3,command=lambda: words(text_twentyone,20),bg="light blue")
-    global b_twentytwo
-    
-    b_twentytwo=Button(f_two,text=text_twentytwo,width=13,height=3,command=lambda: words(text_twentytwo,21),bg="light blue")
-    global b_twentythree
-    
-    b_twentythree=Button(f_two,text=text_twentythree,width=13,height=3,command=lambda: words(text_twentythree,22),bg="light blue")
-    global b_twentyfour
-    
-    b_twentyfour=Button(f_two,text=text_twentyfour,width=13,height=3,command=lambda: words(text_twentyfour,23),bg="light blue")
-    global b_twentyfive
 
-    b_twentyfive=Button(f_two,text=text_twentyfive,width=13,height=3,command=lambda: words(text_twentyfive,24),bg="light blue")
-    global b_twentysix
 
-    b_twentysix=Button(f_two,text=text_twentysix,width=13,height=3,command=lambda: words(text_twentysix,25),bg="light blue")
-    global b_twentyseven
+    rows, cols = 6, 8  # Assuming you have 6 rows and 8 columns of buttons
 
-    b_twentyseven=Button(f_two,text=text_twentyseven,width=13,height=3,command=lambda: words(text_twentyseven,26),bg="light blue")
-    global b_twentyeight
+    for row in range(rows):
+        for col in range(cols):
+            letter = ascii_lowercase[random.randrange(len(ascii_lowercase))]
+            button = create_button(letter, row, col)
+            button_list.append(button)
 
-    b_twentyeight=Button(f_two,text=text_twentyeight,width=13,height=3,command=lambda: words(text_twentyeight,27),bg="light blue")
-    global b_twentynine
-
-    b_twentynine=Button(f_two,text=text_twentynine,width=13,height=3,command=lambda: words(text_twentynine,28),bg="light blue")
-    global b_thirty
-
-    b_thirty=Button(f_two,text=text_thirty,width=13,height=3,command=lambda: words(text_thirty,29),bg="light blue")
-    global b_thirtyone
-
-    b_thirtyone=Button(f_two,text=text_thirtyone,width=13,height=3,command=lambda: words(text_thirtyone,30),bg="light blue")
-    global b_thirtytwo
-
-    b_thirtytwo=Button(f_two,text=text_thirtytwo,width=13,height=3,command=lambda: words(text_thirtytwo,31),bg="light blue")
-    global b_thirtythree
-
-    b_thirtythree=Button(f_two,text=text_thirtythree,width=13,height=3,command=lambda: words(text_thirtythree,32),bg="light blue")
-    global b_thirtyfour
-
-    b_thirtyfour=Button(f_two,text=text_thirtyfour,width=13,height=3,command=lambda: words(text_thirtyfour,33),bg="light blue")
-    global b_thirtyfive
-
-    b_thirtyfive=Button(f_two,text=text_thirtyfive,width=13,height=3,command=lambda: words(text_thirtyfive,34),bg="light blue")
-    global b_thirtysix
-
-    b_thirtysix=Button(f_two,text=text_thirtysix,width=13,height=3,command=lambda: words(text_thirtysix,35),bg="light blue")
-    global b_thirtyseven
-
-    b_thirtyseven=Button(f_two,text=text_thirtyseven,width=13,height=3,command=lambda: words(text_thirtyseven,36),bg="light blue")
-    global b_thirtyeight
-
-    b_thirtyeight=Button(f_two,text=text_thirtyeight,width=13,height=3,command=lambda: words(text_thirtyeight,37),bg="light blue")
-    global b_thirtynine
-
-    b_thirtynine=Button(f_two,text=text_thirtynine,width=13,height=3,command=lambda: words(text_thirtynine,38),bg="light blue")
-    global b_forty
-
-    b_forty=Button(f_two,text=text_forty,width=13,height=3,command=lambda: words(text_forty,39),bg="light blue")
-    global b_fortyone
-
-    b_fortyone=Button(f_two,text=text_fortyone,width=13,height=3,command=lambda: words(text_fortyone,40),bg="light blue")
-    global b_fortytwo
-
-    b_fortytwo=Button(f_two,text=text_fortytwo,width=13,height=3,command=lambda: words(text_fortytwo,41),bg="light blue")
-    global b_fortythree3
-
-    b_fortythree=Button(f_two,text=text_fortythree,width=13,height=3,command=lambda: words(text_fortythree,42),bg="light blue")
-    global b_fortyfour
-
-    b_fortyfour=Button(f_two,text=text_fortyfour,width=13,height=3,command=lambda: words(text_fortyfour,43),bg="light blue")
-    
-    
-    b_one.place(x=10,y=150)
-    b_two.place(x=110,y=150)
-    b_three.place(x=210,y=150)
-    b_four.place(x=310,y=150)
-    b_five.place(x=410,y=150)
-    b_six.place(x=510,y=150)
-    b_seven.place(x=610,y=150)
-    b_eight.place(x=710,y=150)
-    b_ten.place(x=10,y=200)
-    b_eleven.place(x=110,y=200)
-    b_twelve.place(x=210,y=200)
-    b_thirteen.place(x=310,y=200)
-    b_fourteen.place(x=410,y=200)
-    b_fifteen.place(x=510,y=200)
-    b_sixteen.place(x=610,y=200)
-    b_seventeen.place(x=710,y=200)
-    b_nineteen.place(x=10,y=250)
-    b_twenty.place(x=110,y=250)
-    b_twentyone.place(x=210,y=250)
-    b_twentytwo.place(x=310,y=250)
-    b_twentythree.place(x=410,y=250)
-    b_twentyfour.place(x=510,y=250)
-    b_twentyfive.place(x=610,y=250)
-    b_twentysix.place(x=710,y=250)
-    b_twentyeight.place(x=10,y=300)
-    b_twentynine.place(x=110,y=300)
-    b_thirty.place(x=210,y=300)
-    b_thirtyone.place(x=310,y=300)
-    b_thirtytwo.place(x=410,y=300)
-    b_thirtythree.place(x=510,y=300)
-    b_thirtyfour.place(x=610,y=300)
-    b_thirtyfive.place(x=710,y=300)
-    b_thirtyseven.place(x=10,y=350)
-    b_thirtyeight.place(x=110,y=350)
-    b_thirtynine.place(x=210,y=350)
-    b_forty.place(x=310,y=350)
-    b_fortyone.place(x=410,y=350)
-    b_fortytwo.place(x=510,y=350)
-    b_fortythree.place(x=610,y=350)
-    b_fortyfour.place(x=710,y=350)
     
 
-    Submit_button=Button(f_two,text="Submit",font="Courier 9 bold",width=13,height=3,command=Submit,activebackground="yellow",bg="black",borderwidth=0,fg="gold",highlightthickness=2,highlightcolor="gold",default="active")
+    Submit_button=Button(f_two, text="Submit", font="Courier 9 bold", width=13, height=3, command=submit, activebackground="yellow", bg="black", borderwidth=0, fg="gold", highlightthickness=2, highlightcolor="gold", default="active")
     Submit_button.place(x=350,y=540)
-    global button_list
-    button_list=[b_one,b_two,b_three,b_four,b_five,b_six,b_seven,b_eight,b_nine,b_ten,b_eleven,b_twelve,b_thirteen,b_fourteen,b_fifteen,b_sixteen,b_seventeen,b_eighteen,b_nineteen,b_twenty,b_twentyone,b_twentytwo,b_twentythree,b_twentyfour,b_twentyfive,b_twentysix,b_twentyseven,b_twentyeight,b_twentynine,b_thirty,b_thirtyone,b_thirtytwo,b_thirtythree,b_thirtyfour,b_thirtyfive,b_thirtysix,b_thirtyseven,b_thirtyeight,b_thirtynine,b_forty,b_fortyone,b_fortytwo,b_fortythree,b_fortyfour]
-    
+
     global Reset_Button
     Reset_Button=Button(f_two,text="Reset",font="Courier 9 bold",command=Reset,width=13,height=3,activebackground="gold",borderwidth=0,bg="black",fg="gold",highlightthickness=2,highlightcolor="gold",default="active")
     Reset_Button.place(x=550,y=540)
@@ -266,6 +72,7 @@ def play():
     with open(r"Max_score.txt",'r') as maxi:
         global ms
         ms=maxi.readline()
+
     maxscore_label=Label(f_two,text=f"Max Score: {ms}",font="Courier 13 bold",fg="light green",bg="black")
     maxscore_label.place(x=650,y=50)
     
@@ -277,10 +84,11 @@ def play():
 def Popup():
     response=messagebox.askyesno("Quit","Are you sure you want to quit?")
     if response==1:
-        root.quit()
+        quit()
     else:
         pass
-reset_counter=0
+
+
 def Reset():
     global reset_counter
     if reset_counter<5:
@@ -307,12 +115,7 @@ def words(input,num):
     button_list[position]['state']=DISABLED
 
 
-
-
-
-
-
-def Submit():
+def submit():
 
     # global e_one
     global reset_counter
@@ -331,6 +134,9 @@ def Submit():
         if Check:
             if len(word) == 1:
                 wrong_label.place(x=390,y=15)
+                Score += 0
+
+                
             else:
                 correct_label.place(x=390,y=15)
                 Score += len(word)
@@ -373,7 +179,10 @@ def Submit():
 def Clear():
     global e_one
     # global position
+    e_one.config(state='normal')
     e_one.delete(0,END)
+    e_one.config(state='readonly')
+
     global button_list
     for i in  range(44):
         for j in range(44):
@@ -384,7 +193,6 @@ def Clear():
             pass
         
 
-colour_list=["red","light blue","light green","violet","magenta","white","yellow","blue","indigo","cyan","pink","green","orange"]
 
 def colour_change(colour_index):
     logo_label.configure(fg=colour_list[colour_index])
@@ -393,10 +201,7 @@ def colour_change(colour_index):
         colour_index=0
     f_one.after(1000,colour_change,colour_index)
 
-frames = [
-    PhotoImage(file=r"welcome.gif", format="gif -index %i" % (i))
-    for i in range(2)
-]
+
 def update(ind):
     frame = frames[ind]
     ind += 1
@@ -406,10 +211,13 @@ def update(ind):
     f_one.after(200, update, ind)
 
 
-ms=0
-submit_count=0
-Score=0
-Score_string=f"Score: {Score}"
+def quit():
+    global button_list
+    button_list = []
+    root.quit()
+
+
+
 
 
 f_one=LabelFrame(root,width=820,height=700,bg="black")
@@ -436,7 +244,7 @@ Play_button=Button(f_one, text="Play", font="Courier 20 bold", command=play, fg=
 Play_button.place(x=370,y=400)
 
 
-Exit_button=Button(f_one,text="Exit",command=root.quit,bg="black",font="Courier 20 bold",activebackground="light blue",borderwidth=0,fg="light blue",highlightthickness=1,highlightbackground="light blue",highlightcolor="light blue",default="active")
+Exit_button=Button(f_one,text="Exit",command=quit,bg="black",font="Courier 20 bold",activebackground="light blue",borderwidth=0,fg="light blue",highlightthickness=1,highlightbackground="light blue",highlightcolor="light blue",default="active")
 Exit_button.place(x=373,y=500)
 
 f_one.after(0,colour_change,0)    
